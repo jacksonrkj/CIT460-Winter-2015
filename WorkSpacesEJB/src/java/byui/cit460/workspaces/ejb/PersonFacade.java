4756cb01083a870367bba361979fc7bfa9850dd6
Preston <Preston@10.18.150.144>
2015-03-11 10:37:54 -0600

    Update the person facade with authenticate

--- WorkSpacesEJB/src/java/byui/cit460/workspaces/ejb/PersonFacade.java
+++ WorkSpacesEJB/src/java/byui/cit460/workspaces/ejb/PersonFacade.java
@@ -5,6 +5,7 @@
  */
 package byui.cit460.workspaces.ejb;
 
+import byui.cit460.workshops.execeptions.WorkspacesException;
 import byui.cit460.workspaces.data.Person;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
@@ -26,6 +27,11 @@ public class PersonFacade extends AbstractFacade<Person> implements byui.cit460.
 
     public PersonFacade() {
         super(Person.class);
+    }
+
+    @Override
+    public String authenticate(String username, String password) throws WorkspacesException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
