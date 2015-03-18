eebbb607d72abb1e55be622cac1cff86917448a7
Preston <Preston@10.18.151.216>
2015-03-18 11:23:46 -0600

    Authenticate Update bug

--- WorkSpacesEJB/src/java/byui/cit460/workspaces/ejb/PersonFacade.java
+++ WorkSpacesEJB/src/java/byui/cit460/workspaces/ejb/PersonFacade.java
@@ -6,10 +6,13 @@
 package byui.cit460.workspaces.ejb;
 
 
+import byui.cit460.workspaces.data.DocItem;
 import byui.cit460.workspaces.data.Document;
 import byui.cit460.workspaces.data.Person;
 import byui.cit460.workspaces.data.Workspace;
 import byui.cit460.workspaces.exceptions.WorkspacesException;
+import com.google.gson.Gson;
+import java.util.ArrayList;
 import java.util.Collection;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
@@ -48,8 +51,8 @@ public class PersonFacade extends AbstractFacade<Person> implements PersonFacade
    
         Person person = null;
         String portalDocuments = "";
-        Collection<Document> workspaces;
-        Collection<Document> gradesEvents;
+        Collection<Object[]> workspaces;
+        Collection<Object[]> gradesEvents;
 
         if (username == null || password == null) {
             throw new WorkspacesException("Invalid username and/or password");
@@ -74,24 +77,25 @@ public class PersonFacade extends AbstractFacade<Person> implements PersonFacade
         // get all portal documents
         
         // Get the Persons Workspace 
-        Query queryWorkspace = em.createQuery("SELECT r.document FROM Workspace AS w "
-                + "INNER JOIN w.membershipCollection as m "
+        Query queryWorkspace = em.createQuery("SELECT r.document.documentNo, r.document.contextType, r.document.shortDescr FROM Membership AS m "
+                + "INNER JOIN m.workspace as w "
                 + "INNER JOIN w.references as r "
                 + "WHERE m.membershipPK.personId = :personid "
-                + "AND w.workspaceType = 'CTIG'");
+                + "AND w.workspaceType = 'GTIG' "
+                + "ORDER BY r.document.shortDescr");
         
        queryWorkspace.setParameter("personid", person.getPersonId()); 
        
        
        
        try{
-           workspaces = (Collection<Document>) queryWorkspace.getResultList();
+           workspaces = (Collection<Object[]>) queryWorkspace.getResultList();
         }
         catch (Exception e) { // catch all other exceptions and throw custom exception
             throw new WorkspacesException(e.getMessage());
         }
        
-       Query grades = em.createQuery("SELECT r.document FROM Membership AS m "
+       Query grades = em.createQuery("SELECT DISTINCT r.document.documentNo, r.document.contextType, r.document.shortDescr FROM Membership AS m "
                + "INNER JOIN m.workspace AS w "
                + "INNER JOIN w.references AS r "
                + "WHERE m.membershipPK.personId = :personId "
@@ -101,13 +105,81 @@ public class PersonFacade extends AbstractFacade<Person> implements PersonFacade
        grades.setParameter("personId", person.getPersonId());
        
        try{
-           gradesEvents = (Collection<Document>) grades.getResultList();
+           gradesEvents = (Collection<Object[]>) grades.getResultList();
        }
        catch (Exception e) { // catch all other exceptions and throw custom exception
             throw new WorkspacesException(e.getMessage());
         }
-              
-         
+       
+       PortalInfo portalDocs = new PortalInfo(); 
+       portalDocs.setUser(person);
+       ArrayList<DocItem> sections = new ArrayList<>(); 
+       ArrayList<DocItem> userGrades = new ArrayList<>(); 
+       ArrayList<DocItem> events = new ArrayList<>(); 
+        for (Object[] workspaceObj : workspaces) {
+            String[] workspace = (String[]) workspaceObj;
+            sections.add(new DocItem(workspace[0], workspace[2]));
+        }
+        
+        for(Object[] gradeEventObj: gradesEvents){
+            String[] gradeEvent = (String[]) gradeEventObj;
+            if(gradeEvent[1].equals("CTFG")){
+                userGrades.add(new DocItem(gradeEvent[0], gradeEvent[1]));
+            }
+            else
+                events.add(new DocItem(gradeEvent[0], gradeEvent[1]));
+        }
+       
+       portalDocs.setSections(sections);
+       portalDocs.setEvents(events);
+       portalDocs.setGrades(userGrades);
+       Gson json = new Gson(); 
+       portalDocuments = json.toJson(portalDocs);
+       
         return portalDocuments;
     }
+    
+    class PortalInfo{
+        Person user; 
+        ArrayList<DocItem> sections; 
+        ArrayList<DocItem> grades; 
+        ArrayList<DocItem> events; 
+
+        public PortalInfo() {
+        }
+
+        public Person getUser() {
+            return user;
+        }
+
+        public void setUser(Person user) {
+            this.user = user;
+        }
+
+        public ArrayList<DocItem> getSections() {
+            return sections;
+        }
+
+        public void setSections(ArrayList<DocItem> sections) {
+            this.sections = sections;
+        }
+
+        public ArrayList<DocItem> getGrades() {
+            return grades;
+        }
+
+        public void setGrades(ArrayList<DocItem> grades) {
+            this.grades = grades;
+        }
+
+        public ArrayList<DocItem> getEvents() {
+            return events;
+        }
+
+        public void setEvents(ArrayList<DocItem> events) {
+            this.events = events;
+        }
+        
+    }
+    
 }
