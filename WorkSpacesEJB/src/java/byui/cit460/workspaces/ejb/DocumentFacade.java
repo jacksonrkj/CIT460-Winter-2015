632e5a18349fff4a3f6828b85485fdef8686f3fa
Shane Starr <starr749@gmail.com>
2015-03-23 11:07:04 -0600

    Added retrieveAssignments and test function

--- WorkSpacesEJB/src/java/byui/cit460/workspaces/ejb/DocumentFacade.java
+++ WorkSpacesEJB/src/java/byui/cit460/workspaces/ejb/DocumentFacade.java
@@ -6,10 +6,22 @@
 package byui.cit460.workspaces.ejb;
 
 import byui.cit460.workspaces.data.Document;
+import byui.cit460.workspaces.data.Workspace;
+import byui.cit460.workspaces.data.Person;
+import byui.cit460.workspaces.exceptions.WorkspacesException;
+import com.google.gson.Gson;
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.Collection;
+import java.util.HashMap;
 
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 /**
  *
  * @author jacksonrkj
@@ -27,5 +39,70 @@ public class DocumentFacade extends AbstractFacade<Document> implements Document
     public DocumentFacade() {
         super(Document.class);
     }
-    
+ 
+    public String retrieveAssignments(Person user, Workspace workspace) throws WorkspacesException {
+        
+        if (workspace == null) {
+            throw new WorkspacesException("Invalid section");
+        }
+        
+        ArrayList<Object> assignments;
+        
+        Query queryAssignments = em.createQuery("SELECT document_id, document_no, short_descr, doc_text, doc_url, version_no FROM Document AS d"
+                + "INNER JOIN d.references as r"
+                + "INNER JOIN r.workspace as w"
+                + "WHERE w.workspace_id = :workspaceId "
+                + "AND d.context_type = 'CTAS' "
+                + "ORDER BY r.displayOrder");
+        
+        queryAssignments.setParameter("workspaceId", workspace.getWorkspaceId());
+        
+        try{
+           assignments = (ArrayList<Object>) queryAssignments.getResultList();
+        }
+        catch (Exception e) { // catch all other exceptions and throw custom exception
+            throw new WorkspacesException(e.getMessage());
+        }
+        
+        PortalInfo portalInfo = new PortalInfo();
+        portalInfo.setPersonId(user.getPersonId());
+        portalInfo.getDocuments().put(workspace.getDescription(), assignments);
+        
+        return portalInfo.toJson();
+    }
+        class PortalInfo{
+
+        private BigDecimal personId; 
+        private HashMap<String, ArrayList<Object>> documents = new HashMap<>();     
+
+        public PortalInfo() {
+        }
+
+        public BigDecimal getPersonId() {
+            return personId;
+        }
+
+        public void setPersonId(BigDecimal personId) {
+            this.personId = personId;
+        }
+
+
+        
+
+        public HashMap<String, ArrayList<Object>> getDocuments() {
+            return documents;
+        }
+
+        public void setDocuments(HashMap<String, ArrayList<Object>> documents) {
+            this.documents = documents;
+        }
+
+        
+        public String toJson() {
+            Gson gson = new Gson();
+            String jsonString = gson.toJson(this);
+            return jsonString;
+        }
+        
+    }
 }
