1c03ddad0eb76e3d326fe5950b4ecb1381b24b79
R. Kent Jackson <jacksonk@byui.edu>
2015-03-23 10:54:30 -0600

    3/23/2015 - Added stubs for WorkspaceFacade functions

--- WorkSpacesEJB/src/java/byui/cit460/workspaces/ejb/WorkspaceFacade.java
+++ WorkSpacesEJB/src/java/byui/cit460/workspaces/ejb/WorkspaceFacade.java
@@ -5,7 +5,9 @@
  */
 package byui.cit460.workspaces.ejb;
 
+import byui.cit460.workspaces.data.Workgroup;
 import byui.cit460.workspaces.data.Workspace;
+import byui.cit460.workspaces.exceptions.WorkspacesException;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -26,6 +28,41 @@ public class WorkspaceFacade extends AbstractFacade<Workspace> implements Worksp
 
     public WorkspaceFacade() {
         super(Workspace.class);
+    }
+
+    @Override
+    public String addGroup(Workgroup newGroup) throws WorkspacesException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String viewGroup(int personId, int workspaceId) throws WorkspacesException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String deleteGroup(int personId, int workspaceId) throws WorkspacesException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String addMember(int personId, int workspaceId, int newMember) throws WorkspacesException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String removeMember(int personId, int workspaceId, int removeMember) throws WorkspacesException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String changeDesc(int personId, int workspaceId, String desc) throws WorkspacesException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String changeSize(int personId, int workspaceId, Integer maxSize) throws WorkspacesException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
